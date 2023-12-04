544ae4f31872987253df4af4cf60cf68594525ff
Zach Dawson <zach.a.dawson@gmail.com>
2023-12-04 14:07:22 -0600

    Remove unnecessary import

--- day4/Day4.java
+++ day4/Day4.java
@@ -5,7 +5,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 
 public class Day4 {
