49db72f67f8655eb5a803ab5acf3ca31e13b6a28
Zach Dawson <zach.a.dawson@gmail.com>
2023-12-06 17:58:26 -0600

    Give up (brute force ftw)
    Solve day 5 part 2 using brute force
    
    Added a counter for my sanity

--- day5/Day5.java
+++ day5/Day5.java
@@ -14,7 +14,7 @@ public class Day5 {
         // Convert the almanac into a list of seeds and a list of maps
         String[] almanacParts = almanac.split("\n\n");
         String[] seeds = almanacParts[0].replaceFirst("seeds: ", "").split(" ");
-        String[][] maps = new String[7][];
+        double[][][] maps = new double[7][][];
         for (int i = 1; i < 8; i++) {
             maps[i - 1] = parseMap(almanacParts[i]);
         }
@@ -23,7 +23,7 @@ public class Day5 {
         double minLocation = Double.MAX_VALUE;
         for (String seed : seeds) {
             double src = Double.parseDouble(seed);
-            for (String[] map : maps) {
+            for (double[][] map : maps) {
                 double dst = getDst(map, src);
                 if (dst != -1) {
                     src = dst;
@@ -36,12 +36,19 @@ public class Day5 {
         // Part 2
         minLocation = Double.MAX_VALUE;
         double[][] seedsWithRanges = getSeedsWithRanges(seeds);
+        double totalSeeds = 0;
+        for (double[] seedAndRange : seedsWithRanges) {
+            totalSeeds += seedAndRange[0] + seedAndRange[1] - 1;
+        }
         for (double[] seedAndRange : seedsWithRanges) {
             double startSeed = seedAndRange[0];
             double range = seedAndRange[1];
-            for (double seed = startSeed; seed < startSeed + range; seed++) {
+            double endSeed = startSeed + range;
+            for (double seed = startSeed; seed < endSeed; seed++) {
+                totalSeeds--;
+                System.out.print(String.format("%.0f", totalSeeds) + " seeds left \r");
                 double src = seed;
-                for (String[] map : maps) {
+                for (double[][] map : maps) {
                     double dst = getDst(map, src);
                     if (dst != -1) {
                         src = dst;
@@ -53,18 +60,25 @@ public class Day5 {
         System.out.println("Part Two Answer: " + String.format("%.0f", minLocation));
     }
 
-    private static String[] parseMap(String mapString) {
-        // Remove first line of mapString and split into an array
-        return mapString.replaceFirst(".*\n", "").split("\n");
+    private static double[][] parseMap(String mapString) {
+        // Turn mapString into a 2D array
+        String[] mapLines = mapString.replaceFirst(".*\n", "").split("\n");
+        double[][] map = new double[mapLines.length][3];
+        for (int i = 0; i < mapLines.length; i++) {
+            String[] line = mapLines[i].split(" ");
+            for (int j = 0; j < 3; j++) {
+                map[i][j] = Double.parseDouble(line[j]);
+            }
+        }
+        return map;
     }
 
-    private static double getDst(String[] map, double src) {
+    private static double getDst(double[][] map, double src) {
         // Get destination given the source and map
         for (int i = 0; i < map.length; i++) {
-            String[] splitMapLine = map[i].split(" ");
-            double dstStart = Double.parseDouble(splitMapLine[0]);
-            double srcStart = Double.parseDouble(splitMapLine[1]);
-            double range = Double.parseDouble(splitMapLine[2]);
+            double dstStart = map[i][0];
+            double srcStart = map[i][1];
+            double range = map[i][2];
             double distanceFromStart = src - srcStart;
             if (distanceFromStart >= 0 && distanceFromStart < range) {
                 return dstStart + distanceFromStart;
