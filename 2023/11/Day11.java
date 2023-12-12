cedce5cde27552766ea1def400f307508a2d62b7
Zach Dawson <zach.a.dawson@gmail.com>
2023-12-12 12:52:53 -0600

    Day 11 Complete!

--- 2023/11/Day11.java
+++ 2023/11/Day11.java
@@ -21,22 +21,60 @@ public class Day11 {
 
         // Loop through "image"
         for (int i = 0; i < lines.size(); i++) {
+            List<List<Integer>> newGalaxies = new ArrayList<>();
             String line = lines.get(i);
-            List<Integer> galaxyIndices = new ArrayList<>();
             for (Integer j = 0; j < line.length(); j++) {
                 if (line.charAt(j) == '#') {
-                    galaxyIndices.add(j);
+                    List<Integer> coords = new ArrayList<>();
+                    coords.add(i);
+                    coords.add(j);
+                    newGalaxies.add(coords);
                     emptyCols.remove(j);
                 }
             }
-            if (galaxyIndices.isEmpty()) {
+            if (newGalaxies.isEmpty()) {
                 emptyRows.add(i);
             }
-            galaxies.add(galaxyIndices);
+            galaxies.addAll(newGalaxies);
         }
 
-        System.out.println(galaxies);
-        System.out.println(emptyRows);
-        System.out.println(emptyCols);
+        int partOneLength = 0;
+        long partTwoLength = 0;
+        for (int i = 0; i < galaxies.size() - 1; i++) {
+            List<Integer> galaxyA = galaxies.get(i);
+            int rowA = galaxyA.get(0);
+            int colA = galaxyA.get(1);
+            for (int j = i + 1; j < galaxies.size(); j++) {
+                List<Integer> galaxyB = galaxies.get(j);
+                int rowB = galaxyB.get(0);
+                int colB = galaxyB.get(1);
+                // Count rows between
+                int startRow = rowA < rowB ? rowA : rowB;
+                int endRow = rowA >= rowB ? rowA : rowB;
+                for (int k = startRow; k < endRow; k++) {
+                    if (emptyRows.contains(k)) {
+                        partOneLength += 2;
+                        partTwoLength += 1000000;
+                    } else {
+                        partOneLength++;
+                        partTwoLength++;
+                    }
+                }
+                // Count columns between
+                int startCol = colA < colB ? colA : colB;
+                int endCol = colA >= colB ? colA : colB;
+                for (int k = startCol; k < endCol; k++) {
+                    if (emptyCols.contains(k)) {
+                        partOneLength += 2;
+                        partTwoLength += 1000000;
+                    } else {
+                        partOneLength++;
+                        partTwoLength++;
+                    }
+                }
+            }
+        }
+        System.out.println("Part One Answer: " + partOneLength);
+        System.out.println("Part Two Answer: " + partTwoLength);
     }
 }
