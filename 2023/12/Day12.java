c5bf1c672edb4fa668fa3eb2c7016645c624e157
Zach Dawson <zach.a.dawson@gmail.com>
2023-12-20 10:44:34 -0600

    More work for day 12

--- 2023/12/Day12.java
+++ 2023/12/Day12.java
@@ -2,8 +2,12 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class Day12 {
     public static void main(String[] args) throws IOException {
@@ -14,15 +18,22 @@ public class Day12 {
         int totalArrangements = 0;
         for (String line : lines) {
             String[] lineParts = line.split(" ");
-            char[] springs = lineParts[0].toCharArray();
-            int[] damaged = Arrays.stream(lineParts[1].split(",")).mapToInt(Integer::parseInt).toArray();
+            String springs = lineParts[0];
+            List<Integer> damaged = Arrays.stream(lineParts[1].split(",")).map(Integer::valueOf)
+                    .collect(Collectors.toList());
             totalArrangements += countArrangements(springs, damaged);
         }
         System.out.println("Part One Answer: " + totalArrangements);
     }
 
-    private static int countArrangements(char[] springs, int[] damaged) {
-        System.out.println(Arrays.toString(springs) + " " + Arrays.toString(damaged));
+    private static int countArrangements(String springs, List<Integer> damaged) {
+        List<String> unknownSpringGroups = new ArrayList<>();
+        Pattern unknownSpringPattern = Pattern.compile("([\\?#]+)");
+        Matcher unknownSpringMatcher = unknownSpringPattern.matcher(springs);
+        while (unknownSpringMatcher.find()) {
+            unknownSpringGroups.add(unknownSpringMatcher.group());
+        }
+        System.out.println(unknownSpringGroups);
         return 0;
     }
 }
