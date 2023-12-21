1b317b93370ffd370e6951654013b79bd600d9e6
Zach Dawson <zach.a.dawson@gmail.com>
2023-12-21 17:17:07 -0600

    Day 12 Part 1 Complete!

--- 2023/12/Day12.java
+++ 2023/12/Day12.java
@@ -2,38 +2,76 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 public class Day12 {
     public static void main(String[] args) throws IOException {
         // Read input
-        Path inputPath = Paths.get("2023/12/test.txt");
+        Path inputPath = Paths.get("2023/12/input.txt");
         List<String> lines = Files.readAllLines(inputPath);
 
-        int totalArrangements = 0;
+        int partOneTotal = 0;
+        int partTwoTotal = 0;
         for (String line : lines) {
             String[] lineParts = line.split(" ");
             String springs = lineParts[0];
-            List<Integer> damaged = Arrays.stream(lineParts[1].split(",")).map(Integer::valueOf)
-                    .collect(Collectors.toList());
-            totalArrangements += countArrangements(springs, damaged);
+            int[] damagedGroupLengths = Arrays.stream(lineParts[1].split(",")).mapToInt(Integer::parseInt).toArray();
+
+            String[] s = new String[5];
+            int[][] d = new int[5][];
+            for (int i = 0; i < 5; i++) {
+                s[i] = springs;
+                d[i] = damagedGroupLengths;
+            }
+            String unfoldedSprings = String.join("?", s);
+            int[] unfoldedDamagedGroupLengths = Arrays.stream(d)
+                    .flatMapToInt(Arrays::stream)
+                    .toArray();
+
+            partOneTotal += countArrangements(springs, damagedGroupLengths);
+            partTwoTotal += countArrangements(unfoldedSprings, unfoldedDamagedGroupLengths);
         }
-        System.out.println("Part One Answer: " + totalArrangements);
+        System.out.println("Part One Answer: " + partOneTotal);
+        System.out.println("Part Two Answer: " + partTwoTotal);
     }
 
-    private static int countArrangements(String springs, List<Integer> damaged) {
-        List<String> unknownSpringGroups = new ArrayList<>();
-        Pattern unknownSpringPattern = Pattern.compile("([\\?#]+)");
-        Matcher unknownSpringMatcher = unknownSpringPattern.matcher(springs);
-        while (unknownSpringMatcher.find()) {
-            unknownSpringGroups.add(unknownSpringMatcher.group());
+    private static int countArrangements(String springs, int[] damagedGroupLengths) {
+        if (springs.length() == 0) {
+            return damagedGroupLengths.length == 0 ? 1 : 0;
         }
-        System.out.println(unknownSpringGroups);
-        return 0;
+        if (damagedGroupLengths.length == 0) {
+            return springs.indexOf('#') == -1 ? 1 : 0;
+        }
+
+        int arrangements = 0;
+
+        char nextSpring = springs.charAt(0);
+        int damagedGroupLength = damagedGroupLengths[0];
+
+        if (nextSpring == '.' || nextSpring == '?') {
+            arrangements += countArrangements(springs.substring(1, springs.length()), damagedGroupLengths.clone());
+        }
+
+        if (nextSpring == '#' || nextSpring == '?') {
+            boolean enoughSpringsLeft = damagedGroupLength <= springs.length();
+            if (enoughSpringsLeft) {
+                boolean damagedGroupIsContiguous = springs.substring(0, damagedGroupLength).indexOf('.') == -1;
+                if (damagedGroupIsContiguous) {
+                    boolean endsWithOperational = damagedGroupLength == springs.length()
+                            || springs.charAt(damagedGroupLength) != '#';
+                    if (endsWithOperational) {
+                        String nextSprings = damagedGroupLength <= springs.length() - 1
+                                ? springs.substring(damagedGroupLength + 1, springs.length())
+                                : "";
+                        int[] nextDamagedGroupLengths = Arrays.copyOfRange(damagedGroupLengths, 1,
+                                damagedGroupLengths.length);
+                        arrangements += countArrangements(nextSprings, nextDamagedGroupLengths);
+                    }
+                }
+            }
+        }
+
+        return arrangements;
     }
 }
